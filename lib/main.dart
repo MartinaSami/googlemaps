96322076affd227578852c425c93e2bdf58b8ee9
unknown <you@example.com>
2021-07-21 19:40:11 +0200

    googlemaps commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     // title: 'Flutter Google Maps Demo',
+      home: MapSample(),
+    );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+
+  Completer<GoogleMapController> _controller = Completer();
+Set<Marker> myMarkers = {
+  Marker(markerId: MarkerId("1"),position:LatLng(30.013056, 31.208853) ),
+};
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(30.013056, 31.208853),
+    zoom: 14.4746,
+  );
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        markers: myMarkers,
+      ),
+    );
+
+  }
+
+
+}
